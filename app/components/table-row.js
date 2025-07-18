301a574aec21daaad8ab21045aee7e2227037c93
RattatAndyGo <andries66deklerck@gmail.com>
2025-07-18 12:55:23 +0200

    changed table from route to component

--- app/components/table-row.js
+++ app/components/table-row.js
@@ -3,16 +3,28 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class TableRowComponent extends Component {
-    @tracked isSubmitted = false;
+  @tracked isSubmitted = false;
 
-    gameTypes = ["Vraag mee", "Alleen", "Abondance 9", "Abondance 10", "Miserie", "Abondance 11", "Abondance 12", "Troel", "Troela", "Solo", "Solo Slim"]
+  gameTypes = [
+    'Vraag mee',
+    'Alleen',
+    'Abondance 9',
+    'Abondance 10',
+    'Miserie',
+    'Abondance 11',
+    'Abondance 12',
+    'Troel',
+    'Troela',
+    'Solo',
+    'Solo Slim',
+  ];
 
-    gameType = "";
-    gamePlayers = [false, false, false, false];
-    pointsEarned = [0, 0, 0, 0];
-    pointsTotal = [0, 0, 0, 0];
+  gameType = '';
+  gamePlayers = [false, false, false, false];
+  pointsEarned = [0, 0, 0, 0];
+  pointsTotal = [0, 0, 0, 0];
 
-    @action submitGame() {
-        this.isSubmitted = true;
-    }
+  @action submitGame() {
+    this.isSubmitted = true;
+  }
 }
