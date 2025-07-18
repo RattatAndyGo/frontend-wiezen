d590aaa0ccd0f9b229b69d9bb7ea14d883704b99
RattatAndyGo <andries66deklerck@gmail.com>
2025-07-18 14:40:58 +0200

    made player names an input rather than hardcoding it

--- app/components/table.js
+++ app/components/table.js
@@ -3,8 +3,21 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class tableComponent extends Component {
+  @tracked submittedPlayers = false;
+  players = [null, null, null, null];       // The four players
 
   @action
+  submitPlayers(){
+    if(!this.players.includes(null)){
+        this.submittedPlayers = true;
+    }
+  }
+
+  @action
+  // Adds a new hand to the Triplestore and updates UI
+  // gameType is any of "Vraag mee", "Alleen", "Abondance 9"...
+  // gamePlayers is an boolean array of length 4 denoting who took the contract
+  // pointsEarned is an int array of length 4 denoting how many points each player earned
   addNewRow(gameType, gamePlayers, pointsEarned){
     // Calculate new point totals
     // If there is a previous game, add pointsEarned to previous total
